8763b57763ad4f935bf0d37a342a46a0a97c1f30
wjsashen <wan02258@umn.edu>
2025-02-15 14:13:12 -0600

    init and create and TCB construct

--- lib/TCB.cpp
+++ lib/TCB.cpp
@@ -1,11 +1,35 @@
 #include "TCB.h"
 
-TCB::TCB(int tid, void *(*start_routine)(void *arg), void *arg, State state)
+
+#define STACK_SIZE 8192 
+
+TCB::TCB(int tid, Priority pr, void *(*start_routine)(void *), void *arg, State state)
 {
+    getcontext(&_context); // Initialize the thread context
+
+    // Allocate stack memory for the new thread
+    _context.uc_stack.ss_sp = malloc(STACK_SIZE);
+    if (!_context.uc_stack.ss_sp)
+    {
+        std::cerr << "Failed to allocate stack memory\n";
+        exit(1);
+    }
+    _context.uc_stack.ss_size = STACK_SIZE;
+    _context.uc_stack.ss_flags = 0;
+    _context.uc_link = nullptr; // No continuation after thread exit
+
+    // Set up the context to start executing at `stub`, passing function and argument
+    makecontext(&_context, (void (*)())stub, 2, start_routine, arg);
 }
+
+
 
 TCB::~TCB()
 {
+    if (_context.uc_stack.ss_sp)
+    {
+        free(_context.uc_stack.ss_sp);
+    }
 }
 
 void TCB::setState(State state)
@@ -14,10 +38,12 @@ void TCB::setState(State state)
 
 State TCB::getState() const
 {
+    return _state;
 }
 
 int TCB::getId() const
 {
+    return _tid;
 }
 
 void TCB::increaseQuantum()
@@ -26,12 +52,15 @@ void TCB::increaseQuantum()
 
 int TCB::getQuantum() const
 {
+	return this->_quantum;
 }
 
 int TCB::saveContext()
 {
+    return getcontext(&_context);  // Save the current execution context
 }
 
 void TCB::loadContext()
 {
+    setcontext(&_context);  // Restore the saved execution context
 }
